8d78f6042c0f1548779866d419dd5d68085c8d48
allam <allamedd37@gmail.com>
2023-03-27 01:48:34 +0000

    customize css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -98,7 +98,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/allamed/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
